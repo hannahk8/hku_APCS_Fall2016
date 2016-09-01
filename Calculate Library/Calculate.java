9f090e20184efeff2ab174dea5bbfd73d8a59956
hannahk8 <18kuh23@millshighsch.org>
2016-09-01 09:49:48 -0700

    started calculate library part 2

--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -28,14 +28,67 @@ public class Calculate {
 		return((b*b)-(4*a*c));
 	}
 	
-	public static string toImproperFrac(int wholeNum, int numerator, int denom){
+	public static String toImproperFrac(int wholeNum, int numerator, int denom){
 		return(((wholeNum*denom)+numerator)+ "/" + denom);
 	}
 	
-	public static string toMixedNum(int numerator, int denominator){
+	public static String toMixedNum(int numerator, int denominator){
 		int remainder = numerator%denominator;
 		return((numerator - remainder)/denominator + "_" + 
 				numerator%denominator + "/" + denominator);
+	}
+	
+	public static String foil(int a, int b, int c, int d, String x){
+		return((a*c) + x + "^2" + "+" + (c*d) + (a*d) + x + "+" + (b*d) );
+	}
+	
+	//part two
+	public static boolean isDivisibleBy(int a, int b){
+		if(a % b == 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public static double absValue(double num){
+		if(num >= 0){
+			return num;
+		}else{
+			return num + (num * -1);
+		}
+	}
+	
+	public static int max(int a, int b){
+		if(a > b){
+			return a;
+		}else{
+			return b;
+		}
+	}
+	
+	public static double max(double a, double b, double c){
+		if(a > b && a > c){
+			return a; 
+		}
+		if(b > a && b > c){
+			return b;
+		}
+		if(c > a && c > b){
+			return c;
+		}
+	}
+	
+	public static int min(int a, int b){
+		if(a > b){
+			return b;
+		}else{
+			return a;
+		}
+	}
+	
+	public static double round2(double num){
+		if()
 	}
 }
 
